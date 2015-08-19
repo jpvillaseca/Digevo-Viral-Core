f52211ecccb5769059b41f042f4fb3f6692a6a4a
Ricardo Miguel <ricardobeats@gmail.com>
2015-08-19 18:08:33 -0300

    Arcane integration w/ @jpvillaseca
    - Arcane front-end integrated
    - Birthday form's interaction and validation are now done
    - Minor changes

--- Digevo.Viral.Frontend/FuenteDeseo/js/init.js
+++ Digevo.Viral.Frontend/FuenteDeseo/js/init.js
@@ -1,31 +1,65 @@
-﻿/*******************************/
+﻿var day = get('sel1');
+var month = get('sel2');
+var year = get('sel3');
+
+/*******************************/
+/*            Misc             */
+/*******************************/
+
+function get(name) {
+    var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+    if (results == null) {
+        return null;
+    }
+    else {
+        return results[1] || 0;
+    }
+}
+
+function isNumeric(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
+/*******************************/
 /*       Event triggers        */
 /*******************************/
-function onDialogBirthdate(target, form) {
-    //if ($("#sel1").val() <= 0)
-    //    $("#formDay").addClass("has-error");
-    //if ($("#sel2").val() <= 0)
-    //    $("#formMonth").addClass("has-error");
-    //if ($("#sel3").val() <= 0)
-    //    $("#formYear").addClass("has-error");
 
-    //if ($("#sel1").val() > 0 && $("#sel2").val() > 0 && $("#sel3").val() > 0) { //Ready to calculate
-        var birthArcane = getArcane($("#sel1").val() + $("#sel2").val() + $("#sel3").val());
-        var currentArcane = getArcane($("#sel1").val() + $("#sel2").val() + (new Date().getFullYear()));
-        userData.Arcane = arcanes.indexOf(birthArcane);
+//function onDialogBirthdate(target, form) {
+//    if ($("#sel1").val() <= 0)
+//        $("#formDay").addClass("has-error");
+//    if ($("#sel2").val() <= 0)
+//        $("#formMonth").addClass("has-error");
+//    if ($("#sel3").val() <= 0)
+//        $("#formYear").addClass("has-error");
 
-        //window.location = target + "?campaignId=2&birthday=" + birthday + "&bA=" + birthArcane.toString() + "&=cA" + currentArcane.toString();
-        window.location = target + "?" + form;
+//    if ($("#sel1").val() > 0 && $("#sel2").val() > 0 && $("#sel3").val() > 0) { //Ready to calculate
+//        var birthArcane = getArcane($("#sel1").val() + $("#sel2").val() + $("#sel3").val());
+//        var currentArcane = getArcane($("#sel1").val() + $("#sel2").val() + (new Date().getFullYear()));
+//        userData.Arcane = arcanes.indexOf(birthArcane);
 
-        //$("#imgBirthArcane").attr("src", birthArcane.src);
-        //$("#imgCurrentArcane").attr("src", currentArcane.src);
+//        window.location = target + "?campaignId=2&birthday=" + birthday + "&bA=" + birthArcane.toString() + "&=cA" + currentArcane.toString();
+//        window.location = target + "?" + form;
 
-        //$("#dialogBirthdate").toggle();
-        //$("#dialogArcane").toggle();
+//        $("#imgBirthArcane").attr("src", birthArcane.src);
+//        $("#imgCurrentArcane").attr("src", currentArcane.src);
 
-        //save user data
-        //onUserdataChanged(false);
-    //}
+//        $("#dialogBirthdate").toggle();
+//        $("#dialogArcane").toggle();
+
+//        onUserdataChanged(false);
+//    }
+//}
+
+function ready()
+{
+    var birthArcane = getArcane(parseInt(day) + parseInt(month) + parseInt(year));
+    var currentArcane = getArcane(parseInt(day + month) + (new Date().getFullYear()));
+
+    $("#arcano-o").attr("src", birthArcane.src);
+    $("#arcano-a").attr("src", currentArcane.src);
+    
+    userData.Arcane = arcanes.indexOf(birthArcane);
+    onUserdataChanged(false);
 }
 
 function onModalPhoneSend() {
@@ -52,7 +86,7 @@ function onDialogSubmit() {
 
 var arcanes = [{ "titulo": "El MAGO", "src": "img/arc/1.jpg" }, { "titulo": "LA SACERDOTISA", "src": "img/arc/2.jpg" }, { "titulo": "LA EMPERATRIZ", "src": "img/arc/3.jpg" }, { "titulo": "EL EMPERADOR", "src": "img/arc/4.jpg" }, { "titulo": "EL SUMO SACERDOTE", "src": "img/arc/5.jpg" }, { "titulo": "LOS ENAMORADOS", "src": "img/arc/6.jpg" }, { "titulo": "EL CARRO", "src": "img/arc/7.jpg" }, { "titulo": "LA FUERZA", "src": "img/arc/8.jpg" }, { "titulo": "EL HERMITAÑO", "src": "img/arc/9.jpg" }];
 var userData = { "Name": "", "Phone": "", "Birthday": "", "Email": "", "Arcane": "0", "ArcaneQuery": "" };
-var birthday = new Date($("#sel3").val(), ($("#sel2").val() > 0 ? $("#sel2").val() : 1) - 1, $("#sel1").val());
+var birthday = new Date(year, (month > 0 ? month : 1) - 1, day);
 
 function onUserdataChanged(triggerServiceCall) {
     userData.Name = $("#txtName").val();
@@ -65,8 +99,8 @@ function onUserdataChanged(triggerServiceCall) {
         $.ajax({
             type: "POST",
             cache: false,
-            //url: "http://54.207.51.154:3567/api/landing?campaignId=2",
-            url: "http://localhost:3567/api/landing?campaignId=2",
+            url: "http://54.207.51.154:3567/api/landing?campaignId=2",
+            //url: "http://localhost:3567/api/landing?campaignId=2",
             data: "=" + encodeURIComponent(JSON.stringify(userData)),
             crossDomain: true,
             processData: false,
@@ -104,17 +138,14 @@ function getArcane(value) {
 /*******************************/
 
 $(window).load(function () {
-    //Setup values
-    for (var i = 1; i <= 31; i++) {
-        $("#sel1").append(new Option(i, i));
+    if (day == null || month == null || year == null || !isNumeric(day) || !isNumeric(month) || !isNumeric(year))
+        window.location = "index.html";
+    else {
+        $("#date").html(day + "-" + month + "-" + year);
+        ready();
     }
 
-    for (var i = 1900; i < 2016; i++) {
-        $("#sel3").append(new Option(i, i));
-    }
-
-    $("#mobilenumber").intlTelInput(
-      {
+    $("#mobilenumber").intlTelInput({
           onlyCountries: ["co"],
           nationalMode: false,
           autoPlaceholder: true,
